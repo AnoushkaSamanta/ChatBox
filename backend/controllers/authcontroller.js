356979dc252d475c3fb14ecbfd213d6a9d8faa8b
AnoushkaSamanta <anoushka1701@gmail.com>
2025-01-22 23:58:46 +0530

    Authentication

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,45 +1,92 @@
-import User from "./../models/usermodel.js"
-export const signup= async (req,res)=>{
-  try{
-       const {fullname,username,password,confirmPassword,gender}=req.body;
-        if(password!== confirmPassword){
-        return res.status(400).json({error:"Passwords don't match"})
-     }
+import bcrypt from "bcryptjs";
+import User from "./../models/usermodel.js";
+import generateToken from "../utils/generateToken.js";
 
-     const user=await User.findOne({username});
-     if(user){
-        return res.status(400).json({error:"Username already exists"})
-     }
+export const signup = async (req, res) => {
+  try {
+    const { fullname, username, password, confirmPassword, gender } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't match" });
+    }
 
-     //hash password
+    const user = await User.findOne({ username });
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
 
-     const boyProfilePic=`https://avatar.iran.liara.run/public/boy?username=${username}`
-     const girlProfilePic=`https://avatar.iran.liara.run/public/girl?username=${username}`
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-     const newUser=new User({
-        fullname,username,password,gender,profilePic:gender==="male"?boyProfilePic:girlProfilePic
-     })
+    //profile pic
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-     await newUser.save();
+    //saving user in database
+    const newUser = new User({
+      fullname,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
 
-     res.status(201).json({
-        _id:newUser._id,
-        fullname:newUser.fullname,
-        username:newUser.username,
-        profilePic:newUser.profilePic
-             })
+    if (newUser) {
+      //generate jwt token
+      generateToken(newUser._id, res);
+
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullname: newUser.fullname,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(500).json({ error: "Invalid user details" });
+    }
+  } catch (error) {
+    console.log("Error in signup", error.message);
+
+    res.status(500).json({ error: "Internal server error" });
   }
-  catch(error){
+};
 
+export const login = async (req, res) => {
+  try {
+    let { username, password } = req.body;
+    const user = await User.findOne({ username });
+    //checking if password is correct
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      res.status(400).json({ error: "Invalid username or password" });
+    }
+    generateToken(user._id, res);
+    res.status(201).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login", error.message);
+
+    res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
-export const login=(req,res)=>{
-  console.log("Login Page")
-}
+export const logout = (req, res) => {
+  try {
+    //removing cookie
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout", error.message);
 
-export const logout=(req,res)=>{
-  console.log("Logout Page")
-}
-
- 
+    res.status(500).json({ error: "Internal server error" });
+  }
+};

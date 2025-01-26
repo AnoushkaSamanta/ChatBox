dc333354195191b0f05885fbe0e5415e27f821a0
AnoushkaSamanta <anoushka1701@gmail.com>
2025-01-26 12:50:47 +0530

    SignUp Login Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-<>
-<h1 className="text-xl text-red-500 font-bold underline">
-    Hello world!
-  </h1>
- 
-  </>
   
-  )
 
+  return <>
+  <div className="p-4 h-screen flex items-center justify-center">
+    <Signup/>
+    </div></>;
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+<div className="flex flex-col items-center justify-center min-w-[24rem] mx-auto">
+  <div className="w-full p-6 rounded-lg bg-gray-400/200 backdrop-blur-md border">
+    <h1 className="text-3xl font-semibold text-center text-gray-300">Login
+
+      <span className='text-sky-500'> ChatBox</span>
+    </h1>
+    <form action="" className='flex flex-col justify-center items-center '>
+      <div>
+        <label className='label p-2'>
+          <span className='text-gray-300 label-text'>Username</span>
+        </label>
+        <input type="text" placeholder='Enter Username'  className='w-full input input-bordered h-10'/>
+      </div>
+      <div>
+      <label className='label p-2'>
+          <span className='text-gray-300 label-text'>Password</span>
+        </label>
+        <input type="password" placeholder='Enter Password'  className='w-full input input-bordered h-10'/>
+        </div>
+
+      <a href="a" className='text-sm hover:underline hover:text-sky-500 mt-2 inline-block'>Dont't have an account? </a>
+
+      <div>
+      <button className="btn btn-soft btn-info mt-4">Login</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+
+
+  )
+}
+
+export default Login

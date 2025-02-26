8c9b9c9e4067b8270d0a0e01c5a70f914182c852
AnoushkaSamanta <anoushka1701@gmail.com>
2025-02-26 23:00:59 +0530

    Search User

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
 import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
 
 function Message({ message }) {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
   const fromMe = message.senderId === authUser._id;
-  const profilePic=fromMe?authUser.profilePic:selectedConversation?.profilePic;
-  const bubbleBgColor=fromMe?"bg-blue-500":"";
-
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "bg-zinc-800";
+  const formattedTime=extractTime(message.createdAt)
+ 
+  
+  // This class correctly determines the alignment
   const chatClassName = fromMe ? "chat-end" : "chat-start";
-
+  
   return (
-    <div className={`chat ${chatClassName} gap-2 flex flex-col items-end`}>
-      <div className="flex items-center gap-5">
-        <div className={`chat-bubble  px-4 py-2 text-white ${bubbleBgColor}`}>{message.message}</div>
-
-        <img
-          src={profilePic}
-          alt="User Avatar"
-          className="w-6 h-6 rounded-full shadow-md"
-        />
+    <div className={`chat ${chatClassName}`}>
+      <div className="chat-image avatar">
+        <div className="w-6 h-6 rounded-full">
+          <img src={profilePic} alt="User Avatar" className="shadow-md" />
+        </div>
       </div>
-
-      {/* Timestamp */}
-      <span className="text-xs text-gray-500 mt-1">12:45 PM</span>
+      
+      <div className={`chat-bubble ${bubbleBgColor} text-white`}>
+        {message.message}
+      </div>
+      
+      <div className="chat-footer opacity-75 text-xs text-zinc-500 mt-1">
+        {formattedTime}
+      </div>
     </div>
   );
 }

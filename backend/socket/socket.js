c7525397943385d6558fc273fde8961f2ce1ec13
AnoushkaSamanta <anoushka1701@gmail.com>
2025-02-27 23:29:10 +0530

    Online User

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -2,23 +2,31 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
-const app=express();
+const app = express();
 
-const server=http.createServer(app);
-const io=new Server(server,{
-    cors:{
-        origin:["http://localhost:3000"],
-        methods:["GET","POST"]
-    }
-})
-io.on("connection",(socket)=>{
-    console.log("A user connected",socket.id);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
+});
 
-    socket.on("disconnect",()=>{
-        console.log("User disconnected",socket.id);
-        
-    })
-    
-})
+const userSocketMap = {};
 
-export {app,io,server}
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined")
+    userSocketMap[userId]=socket.id;
+
+  io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    socket.on("disconnect", () => {
+      console.log("User disconnected", socket.id);
+      delete userSocketMap[userId];
+      io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    });
+});
+
+export { app, io, server };
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import useConversation from "../../zustand/useConversation";
+import { useSocketContext } from "../../context/SocketContext";
 function Conversation({conversation,lastIdx,emoji}) {
   const {selectedConversation,setSelectedConversation}=useConversation();
 
   const isSelected=selectedConversation?._id===conversation._id;
+const {onlineUsers}=useSocketContext();
+
+const isOnline=onlineUsers.includes(conversation._id)
+
   return (
     <>
       <div className={`flex gap-2 items-center hover:bg-sky-400 rounded p-2 py-1 cursor-pointer
@@ -11,14 +16,14 @@ function Conversation({conversation,lastIdx,emoji}) {
       `}
       onClick={()=>setSelectedConversation(conversation)}
       >
-        <div className="avatar online relative">
+        <div className={`avatar ${isOnline?"online" : ""} relative`}>
           <div className="w-12 rounded-full">
             <img
               src={conversation.profilePic}
               alt="user avatar"
             />
           </div>
-          <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>
+          {isOnline && <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></div>}
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">

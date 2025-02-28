fab9c5aacb629b18119902d49314a16d4106f3b1
AnoushkaSamanta <anoushka1701@gmail.com>
2025-02-28 22:20:05 +0530

    Final commits

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import cors from 'cors';
 import dotenv from "dotenv"
@@ -18,6 +19,8 @@ app.use(cors({
 
 const PORT=process.env.PORT || 5000;
 
+const __dirname=path.resolve();
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
@@ -25,6 +28,12 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
  app.use("/api/users",userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist"))) 
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "/frontend/dist/index.html"))
+})
 
 app.get("/",(req,res)=>{
     res.send("Server is ready here")
@@ -34,6 +43,8 @@ app.get("/",(req,res)=>{
 
 server.listen(PORT, '0.0.0.0', () => {
     connectToMongoDB()
+ console.log(`Server is running on http://localhost:${PORT}`)
+ console.log(process.env.JWT_SECRET);
  
 console.log("NODE_ENV:", process.env.NODE_ENV);
 })
